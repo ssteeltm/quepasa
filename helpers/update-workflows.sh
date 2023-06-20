e315e49859c2da5d1a7fe68a2b0c182ffa3b0e0f
nocodeleaks <codeleaks@proton.me>
2023-06-20 13:46:07 -0300

    workflows

---
+++ helpers/update-workflows.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/usr/bin/n8n import:workflow --input=/opt/quepasa-source/extra/n8n+chatwoot/ --separate
+/usr/bin/n8n update:workflow --id 1008 --active=true
+/usr/bin/n8n update:workflow --id 1009 --active=true
+/usr/bin/n8n update:workflow --id 1010 --active=true
+/usr/bin/n8n update:workflow --id 1011 --active=true
+
+exit 0
