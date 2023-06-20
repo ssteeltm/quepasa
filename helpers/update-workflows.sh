3bdc592224b39bd9c4113180056d56790a790ef8
nocodeleaks <codeleaks@proton.me>
2023-06-20 14:22:05 -0300

    chroot before update

--- helpers/update-workflows.sh
+++ helpers/update-workflows.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd /root/.n8n
 /usr/bin/n8n import:workflow --input=/opt/quepasa-source/extra/n8n+chatwoot/ --separate
 /usr/bin/n8n update:workflow --id 1008 --active=true
 /usr/bin/n8n update:workflow --id 1009 --active=true
