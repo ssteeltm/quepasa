b55224e25e716407994da22b5845119549dd7fba
nocodeleaks <codeleaks@proton.me>
2024-04-11 15:14:27 -0300

    Update mime.go

--- src/library/mime.go
+++ src/library/mime.go
@@ -10,6 +10,7 @@ var MIMEs = map[string]string{
 	"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":       ".xlsx",
 	"application/vnd.openxmlformats-officedocument.wordprocessingml.document": ".docx",
 	"application/zip": ".zip",
+	"image/jpeg":      ".jpeg",
 	"image/webp":      ".webp",
 	"text/xml":        ".xml",
 	"video/mp4":       ".mp4",
