8916401a0a2291e01c944988c2d33ec2b8d0f70a
nocodeleaks <codeleaks@proton.me>
2024-09-17 10:12:49 -0300

    version update

--- src/models/qp_defaults.go
+++ src/models/qp_defaults.go
@@ -3,6 +3,6 @@ package models
 import log "github.com/sirupsen/logrus"
 
 // quepasa build version, if ends with .0 means stable versions.
-const QpVersion = "3.24.914.1438 *Highly Unstable, You Should Not Use That !"
+const QpVersion = "3.24.917.1012 *Barely Unstable, You Should Avoid For Now !"
 
 const QpLogLevel = log.InfoLevel
