0d8c4069f98bc22251dbbe44ad86198f59e3bf92
nocodeleaks <codeleaks@proton.me>
2024-11-01 12:51:28 -0300

    Update docker-entrypoint.sh

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 echo "Container's IP address: `awk 'END{print $1}' /etc/hosts`"
 echo "Working dir: `pwd`"
 
+cp -rf /builder/assets ./
 cp -rf /builder/views ./
 cp -rf /builder/service ./
 
