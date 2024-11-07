4eeb4423f10f40066dcfa2e31f8e802750e7a132
nocodeleaks <codeleaks@proton.me>
2024-11-07 11:17:53 -0300

    Update api_handlers_v3.go

--- src/controllers/api_handlers_v3.go
+++ src/controllers/api_handlers_v3.go
@@ -6,10 +6,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 
-	library "github.com/nocodeleaks/quepasa/library"
-	metrics "github.com/nocodeleaks/quepasa/metrics"
 	models "github.com/nocodeleaks/quepasa/models"
-	whatsapp "github.com/nocodeleaks/quepasa/whatsapp"
 )
 
 const APIVersion3 string = "v3"
@@ -51,8 +48,8 @@ func RegisterAPIV3Controllers(r chi.Router) {
 	r.Get(ControllerPrefixV3+"/receive", ReceiveAPIHandler)
 	r.Post(ControllerPrefixV3+"/attachment", AttachmentAPIHandlerV2)
 
-	r.Get(ControllerPrefixV3+"/download/{messageid}", DownloadControllerV3)
-	r.Get(ControllerPrefixV3+"/download", DownloadControllerV3)
+	r.Get(ControllerPrefixV3+"/download/{messageid}", DownloadController)
+	r.Get(ControllerPrefixV3+"/download", DownloadController)
 
 	// PICTURE INFO | DATA --------------------
 	// ----------------------------------------
@@ -101,85 +98,6 @@ func InformationControllerV3(w http.ResponseWriter, r *http.Request) {
 
 	response.ParseSuccess(server)
 	RespondSuccess(w, response)
-}
-
-//endregion
-
-//endregion
-//region CONTROLLER - DOWNLOAD MESSAGE ATTACHMENT
-
-/*
-<summary>
-
-	Renders route GET "/{{version}}/bot/{{token}}/download/{messageid}"
-
-	Any of then, at this order of priority
-	Path parameters: {messageid}
-	Url parameters: ?messageid={messageid} || ?id={messageid}
-	Header parameters: X-QUEPASA-MESSAGEID = {messageid}
-
-</summary>
-*/
-func DownloadControllerV3(w http.ResponseWriter, r *http.Request) {
-
-	response := &models.QpResponse{}
-
-	server, err := GetServer(r)
-	if err != nil {
-		response.ParseError(err)
-		RespondInterface(w, response)
-		return
-	}
-
-	// Checking for ready state
-	status := server.GetStatus()
-	if status != whatsapp.Ready {
-		err = &ApiServerNotReadyException{Wid: server.GetWId(), Status: status}
-		response.ParseError(err)
-		RespondInterfaceCode(w, response, http.StatusServiceUnavailable)
-		return
-	}
-
-	// Default parameters
-	messageid := GetMessageId(r)
-
-	if len(messageid) == 0 {
-		metrics.MessageSendErrors.Inc()
-		err := fmt.Errorf("empty message id")
-		response.ParseError(err)
-		RespondInterface(w, response)
-		return
-	}
-
-	// Default parameters
-	cache := GetCache(r)
-
-	att, err := server.Download(messageid, cache)
-	if err != nil {
-		response.ParseError(err)
-		RespondInterface(w, response)
-		return
-	}
-
-	var filename string
-
-	// If filename not setted
-	if len(att.FileName) == 0 {
-		exten, ok := library.TryGetExtensionFromMimeType(att.Mimetype)
-		if ok {
-
-			// Generate from mime type and message id
-			filename = fmt.Sprint("; filename=", messageid, exten)
-		}
-	} else {
-		filename = fmt.Sprint("; filename=", att.FileName)
-	}
-
-	// setting header filename
-	w.Header().Set("Content-Disposition", fmt.Sprint("attachment", filename))
-
-	w.WriteHeader(http.StatusOK)
-	w.Write(*att.GetContent())
 }
 
 //endregion
