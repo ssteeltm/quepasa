78e51378c75d4210daf80977f53270a335a850a3
nocodeleaks <codeleaks@proton.me>
2024-11-25 11:44:52 -0300

    version update

--- src/models/qp_defaults.go
+++ src/models/qp_defaults.go
@@ -6,7 +6,7 @@ import (
 )
 
 // quepasa build version, if ends with .0 means stable versions.
-const QpVersion = "3.24.1123.0"
+const QpVersion = "3.24.1125.0"
 
 const QpLogLevel = log.InfoLevel
 
--- src/whatsapp/whatsapp_defaults.go
+++ src/whatsapp/whatsapp_defaults.go
@@ -24,7 +24,7 @@ var WhatsappMIMEAudio = [...]string{"audio/oga", "audio/ogx", "audio/x-mpeg-3", 
 var WhatsappMIMEVideo = [...]string{"video/mp4"}
 
 // Mime types for image messages, tested 3º
-var WhatsappMIMEImage = [...]string{"image/png", "image/jpeg", "image/webp"}
+var WhatsappMIMEImage = [...]string{"image/png", "image/jpeg", "image/jpg", "image/webp"}
 
 // Mime types for document messages, tested 4º
 var WhatsappMIMEDocument = [...]string{
